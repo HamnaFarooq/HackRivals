aeba71aceaa95b136f70af2a12a9bceccc8018d3
HamnaFarooq <hamma.farooq@gmail.com>
2020-09-11 19:00:01 +0545

    front-end designing tweaks

--- resources/views/problem/submit.blade.php
+++ resources/views/problem/submit.blade.php
@@ -5,23 +5,23 @@ Home
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-sm-8">
-      <div class="py-3">
-        Level
-        <div class="progress">
-          <div class="progress-bar bg-warning" role="progressbar" style="width: 15%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-        </div>
+    <div class="row">
+        <div class="col">
+            <div class="py-3">
+                Points
+                <div class="progress">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 15%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
 
-        <div class="py-3">
-          SubLevel
-        <div class="progress">
-          <div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-      </div>
-      <div class="row">
-      <div class="row mt-5">
+            <div class="py-3">
+                Aggregate Points
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
                 <div class="col-sm-4">
                     <div class="card border-info mb-3">
                         <div class="card-header">TestCase 1</div>
@@ -139,19 +139,8 @@ Home
                 </div>
 
             </div>
-      </div>
-
+        </div>
     </div>
-    <div class="col-sm-4">
-    <canvas id="myChart" style="width: 500px; height:100%;"></canvas>
-    </div>
-  </div>
-  <div class="py-3">
-    SubLevel Progress
-  <div class="progress">
-  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-</div>
-  </div>
 </div>
 
 
